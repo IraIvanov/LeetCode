bb5b4836089c4f4235a445a42145c9e97598e097
Роман <ivanov.ra@phystech.edu>
2024-02-02 09:55:23 +0300

    palint fixed

--- palint.go
+++ palint.go
@@ -4,7 +4,20 @@ import "fmt"
 
 func main() {
 
-	num := 121
-	str := string(num)
-	fmt.Print(str)
+	num := 10
+	fmt.Print(isPal(num))
+}
+
+func isPal(num int) bool {
+
+	reversed, x := 0, num
+
+	for ; x > 0; x /= 10 {
+
+		reversed = reversed*10 + x%10
+
+	}
+
+	return reversed == num
+
 }
